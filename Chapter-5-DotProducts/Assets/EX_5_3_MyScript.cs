119569f4737e6c7989a15239491397b61984ab69
vid66 <v.khristoliubov@red-beat.com>
2025-08-17 11:55:46 +0300

    Verify that Vector Projection Is Perpendicular

--- Chapter-5-DotProducts/Assets/EX_5_3_MyScript.cs
+++ Chapter-5-DotProducts/Assets/EX_5_3_MyScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,11 @@ public class EX_5_3_MyScript : MonoBehaviour
     public GameObject P1 = null;   // Position P1
     public GameObject Pt = null;   // Position Pt: test position
     public GameObject Pon = null;  // Position Pon: position on the interval-line
+    private Vector3 _v1 = Vector3.zero;
+    private Vector3 _vt = Vector3.zero;
+    private Vector3 _v1N = Vector3.zero;
+    private Vector3 _vOn = Vector3.zero;
+    private float _d = Single.NaN;
 
     #region For visualizing the vectors
     private MyVector ShowV1;    // V1
@@ -56,36 +62,43 @@ public class EX_5_3_MyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 v1 = P1.transform.localPosition - P0.transform.localPosition;
+        _v1 = P1.transform.localPosition - P0.transform.localPosition;
+        _vt = Pt.transform.localPosition - P0.transform.localPosition;
+        _v1N = _v1.normalized;
+        _d = Vector3.Dot(_vt, _v1N);
+        _vOn = _d * _v1N - _vt;
+        if (_v1.magnitude > float.Epsilon) {
+            Pon.transform.localPosition = Pt.transform.localPosition + _vOn;
 
-        if (v1.magnitude > float.Epsilon)
-        {
-            Vector3 vt = Pt.transform.localPosition - P0.transform.localPosition;
-            Vector3 v1n = v1.normalized;
-            float d = Vector3.Dot(vt, v1n);
-            Vector3 vOn = d * v1n - vt;
-            Pon.transform.localPosition = Pt.transform.localPosition + vOn;
-
-            if ((d >= 0) && (d <= v1.magnitude))
-                Debug.Log("V1.mag=" + v1.magnitude + "  Projected Length=" + d + "  ==> Inside!");
+            if ((_d >= 0) && (_d <= _v1.magnitude))
+                Debug.Log("V1.mag=" + _v1.magnitude + "  Projected Length=" + _d + "  ==> Inside!");
             else
-                Debug.Log("V1.mag=" + v1.magnitude + "  Projected Length=" + d + "  ==> Outside!");
+                Debug.Log("V1.mag=" + _v1.magnitude + "  Projected Length=" + _d + "  ==> Outside!");
         }
+
+        if ((Pt.transform.localPosition - P0.transform.position).magnitude > float.Epsilon) {
+            float v1VonDot = Vector3.Dot(_v1, _vOn);
+            float cosAngle = v1VonDot / (_v1.magnitude * _vOn.magnitude);
+            float angleRad = Mathf.Acos(cosAngle);
+            float angleDeg = angleRad * Mathf.Rad2Deg;
+            Debug.Log("Dot production of Von and V1 = " + v1VonDot + ". Cos of angle between Von and V1 = " + cosAngle + ". Angle = " + angleDeg);
+        }
+
 
         #region  For visualizing the vectors
 
         ShowLine.VectorFromTo(P0.transform.localPosition, P1.transform.localPosition);
-        bool visible = v1.magnitude > float.Epsilon;
+        bool visible = _v1.magnitude > float.Epsilon;
         ShowV1.DrawVector = visible;
         ShowPv.DrawVector = visible;
         ShowPa.DrawVector = visible;
 
         if (visible)
         {
-            Vector3 v1n = v1.normalized;
+            Vector3 v1n = _v1.normalized;
             Vector3 vt = Pt.transform.localPosition - P0.transform.localPosition;
             float d = Vector3.Dot(vt, v1n);
-            if (d >= 0 && d <= v1.magnitude)
+            if (d >= 0 && d <= _v1.magnitude)
                 ShowLine.VectorColor = MyDrawObject.CollisionColor;
             else
                 ShowLine.VectorColor = MyDrawObject.NoCollisionColor;
@@ -95,13 +108,13 @@ public class EX_5_3_MyScript : MonoBehaviour
 
             float after = 0.45f;
             float before = 0.15f;
-            Vector3 pv0 = P0.transform.localPosition - before * v1; ;
-            Vector3 pv1 = P1.transform.localPosition + after * v1;
+            Vector3 pv0 = P0.transform.localPosition - before * _v1; ;
+            Vector3 pv1 = P1.transform.localPosition + after * _v1;
 
-            if (d > (((1f + after) * v1.magnitude) - 1f))
+            if (d > (((1f + after) * _v1.magnitude) - 1f))
                 pv1 = Pon.transform.localPosition + v1n;
 
-            if (d < ((-before * v1.magnitude) + 1f))
+            if (d < ((-before * _v1.magnitude) + 1f))
                 pv0 = Pon.transform.localPosition - v1n;
 
             ShowV1.VectorFromTo(pv0, pv1);
