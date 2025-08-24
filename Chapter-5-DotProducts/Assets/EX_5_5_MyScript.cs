69df8fe91582437411247872fc1a132de30a8dad
vid66 <v.khristoliubov@red-beat.com>
2025-08-24 19:13:25 +0300

    Claude refactoring

--- Chapter-5-DotProducts/Assets/EX_5_5_MyScript.cs
+++ Chapter-5-DotProducts/Assets/EX_5_5_MyScript.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EX_5_5_MyScript : MonoBehaviour {
-    // Positions: to deine the two lines.
-    public GameObject P1, P2; // define the line V1
-    public GameObject Pa, Pb; // define the line Va
-    public GameObject Pd_1; // point on V1 closest to Va
-    public GameObject Pd_a; // point on va closest to V1
+public class EX_5_5_MyScript : MonoBehaviour
+{
+    // Positions: to define the two lines.
+    public GameObject P1, P2;  // define the line V1
+    public GameObject Pa, Pb;  // define the line Va
+    public GameObject Pd_1;    // point on V1 closest to Va
+    public GameObject Pd_a;    // point on va closest to V1
 
     #region For visualizing the line
-
     private MyLineSegment ShowV1, ShowVa, ShowVp;
     private const float kScaleFactor = 0.4f;
-
     #endregion
 
-    void Start() {
-        Debug.Assert(P1 != null); // Verify proper setting in the editor
+    void Start()
+    {
+        Debug.Assert(P1 != null);   // Verify proper setting in the editor
         Debug.Assert(P2 != null);
         Debug.Assert(Pd_1 != null);
         Debug.Assert(Pa != null);
@@ -25,33 +25,35 @@ public class EX_5_5_MyScript : MonoBehaviour {
         Debug.Assert(Pd_a != null);
 
         #region For visualizing the line
-
         // To support visualizing the line
-        ShowV1 = new MyLineSegment {
+        ShowV1 = new MyLineSegment
+        {
             VectorColor = Color.red,
             LineWidth = 0.1f
         };
-        ShowVa = new MyLineSegment {
+        ShowVa = new MyLineSegment
+        {
             VectorColor = Color.blue,
             LineWidth = 0.1f
         };
-        ShowVp = new MyLineSegment {
+        ShowVp = new MyLineSegment
+        {
             VectorColor = Color.black,
             LineWidth = 0.05f
         };
         var sv = UnityEditor.SceneVisibilityManager.instance;
         sv.DisablePicking(Pd_1, true);
         sv.DisablePicking(Pd_a, true);
-
         #endregion
     }
 
-    void Update() {
+    void Update()
+    {
         Vector3 v1 = (P2.transform.localPosition - P1.transform.localPosition);
         Vector3 va = (Pb.transform.localPosition - Pa.transform.localPosition);
 
         if ((v1.magnitude < float.Epsilon) || (va.magnitude < float.Epsilon))
-            return; // will only work with well defined line segments
+            return;  // will only work with well defined line segments
 
         Vector3 va1 = P1.transform.localPosition - Pa.transform.localPosition;
         Vector3 v1n = v1.normalized;
@@ -62,7 +64,7 @@ public class EX_5_5_MyScript : MonoBehaviour {
 
         float d1 = 0f, da = 0f;
 
-        if (!almostParallel) // two lines are not parallel
+        if (!almostParallel)  // two lines are not parallel
         {
             float dot1A1 = Vector3.Dot(v1n, va1);
             float dotAA1 = Vector3.Dot(van, va1);
@@ -72,80 +74,124 @@ public class EX_5_5_MyScript : MonoBehaviour {
 
             d1 /= v1.magnitude;
             da /= va.magnitude;
-            if (d1 < 1 && d1 > 0 && da < 1 && da > 0) {
-                Pd_1.transform.localPosition = P1.transform.localPosition + d1 * v1;
-                Pd_a.transform.localPosition = Pa.transform.localPosition + da * va;
-                float dist = (Pd_1.transform.localPosition - Pd_a.transform.localPosition).magnitude;
-                Debug.Log("d1=" + d1 + " da=" + da + " Distance=" + dist);
-            }
-            else if (d1 < 0 && da < 1 && da > 0) {
-                Pd_1.transform.localPosition = P1.transform.localPosition;
-                Pd_a.transform.localPosition = CalculateClosestPoint(P1.transform.localPosition,
-                    Pa.transform.localPosition, Pb.transform.localPosition);
-                float dist = (Pd_1.transform.localPosition - Pd_a.transform.localPosition).magnitude;
-                Debug.Log("d1=" + d1 + " da=" + da + " Distance=" + dist);
-            }
-            else if (d1 > 1 && da < 1 && da > 0) {
-                Pd_1.transform.localPosition = P2.transform.localPosition;
-                Pd_a.transform.localPosition = CalculateClosestPoint(P2.transform.localPosition,
-                    Pa.transform.localPosition, Pb.transform.localPosition);
-                float dist = (Pd_1.transform.localPosition - Pd_a.transform.localPosition).magnitude;
-                Debug.Log("d1=" + d1 + " da=" + da + " Distance=" + dist);
-            }
-            else if (da > 1 && d1 < 1 && d1 > 0) {
-                Pd_1.transform.localPosition = CalculateClosestPoint(Pb.transform.localPosition,
-                    P1.transform.localPosition, P2.transform.localPosition);
-                Pd_a.transform.localPosition = Pb.transform.localPosition;
-                float dist = (Pd_1.transform.localPosition - Pd_a.transform.localPosition).magnitude;
-                Debug.Log("d1=" + d1 + " da=" + da + " Distance=" + dist);
-            }
-            else if (da < 0 && d1 < 1 && d1 > 0) {
-                Pd_1.transform.localPosition = CalculateClosestPoint(Pa.transform.localPosition,
-                    P1.transform.localPosition, P2.transform.localPosition);
-                Pd_a.transform.localPosition = Pa.transform.localPosition;
-                float dist = (Pd_1.transform.localPosition - Pd_a.transform.localPosition).magnitude;
-                Debug.Log("d1=" + d1 + " da=" + da + " Distance=" + dist);
-            }
+
+            // Clamp parameters to segment bounds
+            d1 = Mathf.Clamp01(d1);
+            da = Mathf.Clamp01(da);
+
+            Pd_1.transform.localPosition = P1.transform.localPosition + d1 * v1;
+            Pd_a.transform.localPosition = Pa.transform.localPosition + da * va;
+
+            float dist = (Pd_1.transform.localPosition - Pd_a.transform.localPosition).magnitude;
+            Debug.Log("Non-parallel: d1=" + d1 + " da=" + da + " Distance=" + dist);
         }
-        else {
-            Debug.Log("Line segments are parallel, special case not handled");
+        else  // Handle parallel lines
+        {
+            Debug.Log("Line segments are parallel - computing shortest distance");
+
+            // For parallel lines, find the shortest distance by checking:
+            // 1. Distance from each endpoint of segment 1 to segment 2
+            // 2. Distance from each endpoint of segment 2 to segment 1
+
+            float minDistance = float.MaxValue;
+            Vector3 closestPoint1 = Vector3.zero;
+            Vector3 closestPoint2 = Vector3.zero;
+
+            // Check P1 to segment Va
+            Vector3 tempClosest = ClosestPointOnSegment(P1.transform.localPosition,
+                Pa.transform.localPosition, Pb.transform.localPosition);
+            float tempDist = (P1.transform.localPosition - tempClosest).magnitude;
+            if (tempDist < minDistance)
+            {
+                minDistance = tempDist;
+                closestPoint1 = P1.transform.localPosition;
+                closestPoint2 = tempClosest;
+            }
+
+            // Check P2 to segment Va
+            tempClosest = ClosestPointOnSegment(P2.transform.localPosition,
+                Pa.transform.localPosition, Pb.transform.localPosition);
+            tempDist = (P2.transform.localPosition - tempClosest).magnitude;
+            if (tempDist < minDistance)
+            {
+                minDistance = tempDist;
+                closestPoint1 = P2.transform.localPosition;
+                closestPoint2 = tempClosest;
+            }
+
+            // Check Pa to segment V1
+            tempClosest = ClosestPointOnSegment(Pa.transform.localPosition,
+                P1.transform.localPosition, P2.transform.localPosition);
+            tempDist = (Pa.transform.localPosition - tempClosest).magnitude;
+            if (tempDist < minDistance)
+            {
+                minDistance = tempDist;
+                closestPoint1 = tempClosest;
+                closestPoint2 = Pa.transform.localPosition;
+            }
+
+            // Check Pb to segment V1
+            tempClosest = ClosestPointOnSegment(Pb.transform.localPosition,
+                P1.transform.localPosition, P2.transform.localPosition);
+            tempDist = (Pb.transform.localPosition - tempClosest).magnitude;
+            if (tempDist < minDistance)
+            {
+                minDistance = tempDist;
+                closestPoint1 = tempClosest;
+                closestPoint2 = Pb.transform.localPosition;
+            }
+
+            Pd_1.transform.localPosition = closestPoint1;
+            Pd_a.transform.localPosition = closestPoint2;
+
+            Debug.Log("Parallel lines: Distance=" + minDistance);
         }
 
-        #region For visualizing the line
-
+        #region  For visualizing the line
         ShowV1.VectorFromTo(P1.transform.localPosition, P2.transform.localPosition);
         ShowVa.VectorFromTo(Pa.transform.localPosition, Pb.transform.localPosition);
-        ShowVp.DrawVector = (!almostParallel);
+        ShowVp.DrawVector = true; // Always show the connection
 
-        Pd_1.SetActive(!almostParallel);
-        Pd_a.SetActive(!almostParallel);
+        Pd_1.SetActive(true);
+        Pd_a.SetActive(true);
 
-        if (!almostParallel) // two lines are not parallel
-        {
-            bool d0Out = (d1 < 0f) || (d1 > 1.0f);
-            bool daOut = (da < 0f) || (da > 1.0f);
-            Color c = Pd_1.GetComponent<Renderer>().material.color;
+        // Update visual properties
+        Color c = Pd_1.GetComponent<Renderer>().material.color;
+        c.a = almostParallel ? 0.8f : 0.25f; // Different alpha for parallel vs non-parallel
+        Pd_1.GetComponent<Renderer>().material.color = c;
+        Pd_a.GetComponent<Renderer>().material.color = c;
 
-            c.a = d0Out ? 1.0f : 0.25f;
-            Pd_1.GetComponent<Renderer>().material.color = c;
-            c.a = daOut ? 1.0f : 0.25f;
-            Pd_a.GetComponent<Renderer>().material.color = c;
+        ShowVp.VectorFromTo(Pd_1.transform.localPosition, Pd_a.transform.localPosition);
 
-            // ShowVp.VectorColor = c;
-            ShowVp.VectorFromTo(Pd_1.transform.localPosition, Pd_a.transform.localPosition);
-
-            float s = ShowVp.Magnitude * kScaleFactor;
-            Pd_1.transform.localScale = new Vector3(s, s, s);
-            Pd_a.transform.localScale = new Vector3(s, s, s);
-        }
-
+        float s = ShowVp.Magnitude * kScaleFactor;
+        Pd_1.transform.localScale = new Vector3(s, s, s);
+        Pd_a.transform.localScale = new Vector3(s, s, s);
         #endregion
     }
 
-    public Vector3 CalculateClosestPoint(Vector3 point, Vector3 a, Vector3 b) {
-        Vector3 ab = b - a;
-        float t = Vector3.Dot(point - a, ab)/ab.magnitude;
-        t = Mathf.Clamp01(t);
-        return a + ab * t;
+    /// <summary>
+    /// Calculate the closest point on a line segment to a given point
+    /// </summary>
+    /// <param name="point">The point to find closest position to</param>
+    /// <param name="segmentStart">Start of the line segment</param>
+    /// <param name="segmentEnd">End of the line segment</param>
+    /// <returns>Closest point on the segment</returns>
+    public Vector3 ClosestPointOnSegment(Vector3 point, Vector3 segmentStart, Vector3 segmentEnd)
+    {
+        Vector3 segmentVector = segmentEnd - segmentStart;
+        Vector3 pointVector = point - segmentStart;
+
+        float segmentLengthSquared = segmentVector.sqrMagnitude;
+
+        if (segmentLengthSquared < float.Epsilon)
+        {
+            // Degenerate segment - start and end are the same
+            return segmentStart;
+        }
+
+        float t = Vector3.Dot(pointVector, segmentVector) / segmentLengthSquared;
+        t = Mathf.Clamp01(t); // Clamp to [0,1] to stay within segment
+
+        return segmentStart + t * segmentVector;
     }
 }
