0f69c0d495db8439b51932cbc59dee2924387862
vid66 <v.khristoliubov@red-beat.com>
2025-10-08 23:26:51 +0300

    The Plane at the Negative Distance

--- Chapter-6-CrossProducts/Assets/EX_6_2_MyScript.cs
+++ Chapter-6-CrossProducts/Assets/EX_6_2_MyScript.cs
@@ -6,22 +6,26 @@ public class EX_6_2_MyScript : MonoBehaviour
 {
     // Defines two vectors: V1 = P1 - P0, V2 = P2 - P0
     public GameObject P0 = null;   // The three positions
-    public GameObject P1 = null;   // 
-    public GameObject P2 = null;   // 
+    public GameObject P1 = null;   //
+    public GameObject P2 = null;   //
 
     // Plane equation:   P dot vn = D
-    public GameObject Ds;         // To show the D-value 
+    public GameObject Ds;         // To show the D-value
     public GameObject Pn;          // Where Vn crosses the plane
 
     public bool ShowPointOnPlane = true;  // Hide or Show Pt
+    public bool ShowOppositeNormal = true;  // Hide or Show Pt
     public GameObject Pt;           // Point to adjust
     public GameObject Pon;          // Point in the Plane, in the Pt direction
+    public GameObject Pd;          // Point in the Plane, in the Pt direction
 
     #region For visualizing the vectors
     private MyVector ShowV1, ShowV2;
     private MyVector ShowVn;
     private MyVector ShowNormal;      // Vn
+    private MyVector ShowOppNormal;
     private MyXZPlane ShowPlane;      // Plane where XZ lies
+    private MyXZPlane ShowOppPlane;      // Plane where XZ lies
     private MyLineSegment ShowPtLine; // Line to Pt
     #endregion
 
@@ -29,12 +33,13 @@ public class EX_6_2_MyScript : MonoBehaviour
     void Start()
     {
         Debug.Assert(P0 != null);   // Verify proper setting in the editor
-        Debug.Assert(P1 != null);   
+        Debug.Assert(P1 != null);
         Debug.Assert(P2 != null);
         Debug.Assert(Ds != null);
         Debug.Assert(Pn != null);
         Debug.Assert(Pt != null);
         Debug.Assert(Pon != null);
+        Debug.Assert(Pd != null);
 
         #region For visualizing the vectors
         // To support visualizing the vectors
@@ -49,6 +54,10 @@ public class EX_6_2_MyScript : MonoBehaviour
         ShowNormal = new MyVector {
             VectorColor = Color.white
         };
+        ShowOppNormal = new MyVector
+        {
+            VectorColor = Color.green
+        };
         ShowVn = new MyVector
         {
             VectorColor = Color.black
@@ -56,6 +65,13 @@ public class EX_6_2_MyScript : MonoBehaviour
         ShowPlane = new MyXZPlane
         {
             PlaneColor = new Color(0.8f, 0.3f, 0.3f, 1.0f),
+            XSize = 0.5f,
+            YSize = 0.5f,
+            ZSize = 0.5f
+        };
+        ShowOppPlane = new MyXZPlane
+        {
+            PlaneColor = new Color(0.2f, 0.5f, 0.8f, 1.0f),
             XSize = 0.5f,
             YSize = 0.5f,
             ZSize = 0.5f
@@ -68,8 +84,9 @@ public class EX_6_2_MyScript : MonoBehaviour
         var sv = UnityEditor.SceneVisibilityManager.instance;
         sv.DisablePicking(Ds, true);
         sv.DisablePicking(Pn, true);
+        sv.DisablePicking(Pd, true);
         sv.DisablePicking(Pon, true);
-        #endregion 
+        #endregion
     }
 
     // Update is called once per frame
@@ -85,9 +102,10 @@ public class EX_6_2_MyScript : MonoBehaviour
         Vector3 vn = Vector3.Cross(v1, v2);
         vn.Normalize();  // keep this vector normalized
         float D = Vector3.Dot(vn, P0.transform.localPosition);
-        
+
         // Showing the plane equation is consistent
         Pn.transform.localPosition = D * vn;
+        Pd.transform.localPosition = D * - vn;
         Ds.transform.localScale = new Vector3(D * 2f, D * 2f, D * 2f); // sphere expects diameter
 
         // Set up for displaying Pt and Pon
@@ -104,7 +122,7 @@ public class EX_6_2_MyScript : MonoBehaviour
             {
                 t = D / d;
                 Pon.transform.localPosition = t * Pt.transform.localPosition;
-            } 
+            }
         }
 
         #region  For visualizing the vectors
@@ -119,8 +137,12 @@ public class EX_6_2_MyScript : MonoBehaviour
         ShowNormal.VectorAt = Vector3.zero;
         ShowNormal.Magnitude = Mathf.Abs(D)+2f;
         ShowNormal.Direction = vn;
+        ShowOppNormal.VectorAt = Vector3.zero;
+        ShowOppNormal.Magnitude = Mathf.Abs(D)+2f;
+        ShowOppNormal.Direction = -vn;
 
         ShowPlane.PlaneNormal = -vn;
+        ShowOppPlane.PlaneNormal = vn;
         Vector3 at = P0.transform.localPosition + P1.transform.localPosition + P2.transform.localPosition + Pn.transform.localPosition;
         int c = 4;
 
@@ -158,6 +180,8 @@ public class EX_6_2_MyScript : MonoBehaviour
 
         ShowPlane.XSize = ShowPlane.ZSize = du;
         ShowPlane.Center = at / c;
+        ShowOppPlane.XSize = ShowOppPlane.ZSize = du;
+        ShowOppPlane.Center = Pd.transform.localPosition;
         #endregion
 
     }
