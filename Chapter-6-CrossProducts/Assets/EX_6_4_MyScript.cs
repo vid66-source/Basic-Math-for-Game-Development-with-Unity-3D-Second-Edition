64325302df56acbef020c535322f41c0112d8942
vid66 <v.khristoliubov@red-beat.com>
2025-10-15 23:25:00 +0300

    Characteristics of the Shadow Casted

--- Chapter-6-CrossProducts/Assets/EX_6_4_MyScript.cs
+++ Chapter-6-CrossProducts/Assets/EX_6_4_MyScript.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EX_6_4_MyScript : MonoBehaviour
-{
+public class EX_6_4_MyScript : MonoBehaviour{
     public bool ShowAxisFrame = true;
     public bool ShowProjections = true;
 
@@ -12,47 +11,45 @@ public class EX_6_4_MyScript : MonoBehaviour
     public float D = 2f;
 
     public GameObject Pn = null;
-    public GameObject Pt = null;  // The point to be projected onto the plane
-    public GameObject Pl = null;  // Projection of Pt on Vn
+    public GameObject Pt = null; // The point to be projected onto the plane
+    public GameObject Pl = null; // Projection of Pt on Vn
     public GameObject Pon = null; // Projection of Pt on the plane
 
     #region For visualizing the vectors
-    private MyVector ShowNormal, ShowPt;    //
+
+    private MyVector ShowNormal, ShowPt; //
     private MyXZPlane ShowPlane; // Plane where XZ lies
     private MyLineSegment ShowPtOnPlane, ShowPtOnN;
+
     #endregion
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Assert(Pn != null);   // Verify proper setting in the editor
+    void Start(){
+        Debug.Assert(Pn != null); // Verify proper setting in the editor
         Debug.Assert(Pt != null);
         Debug.Assert(Pl != null);
         Debug.Assert(Pon != null);
 
         #region For visualizing the vectors
+
         // To support visualizing the vectors
-        ShowNormal = new MyVector {
+        ShowNormal = new MyVector{
             VectorColor = Color.white
         };
-        ShowPlane = new MyXZPlane
-        {
+        ShowPlane = new MyXZPlane{
             PlaneColor = new Color(0.8f, 0.3f, 0.3f, 1.0f),
             XSize = 0.5f,
             YSize = 0.5f,
             ZSize = 0.5f
         };
-        ShowPtOnPlane = new MyLineSegment
-        {
+        ShowPtOnPlane = new MyLineSegment{
             VectorColor = Color.black,
             LineWidth = 0.05f
         };
-        ShowPt = new MyVector
-        {
+        ShowPt = new MyVector{
             VectorColor = Color.red
         };
-        ShowPtOnN = new MyLineSegment
-        {
+        ShowPtOnN = new MyLineSegment{
             VectorColor = Color.green,
             LineWidth = 0.05f
         };
@@ -60,12 +57,12 @@ public class EX_6_4_MyScript : MonoBehaviour
         sv.DisablePicking(Pn, true);
         sv.DisablePicking(Pl, true);
         sv.DisablePicking(Pon, true);
+
         #endregion
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         Vn.Normalize();
         Pn.transform.localPosition = D * this.Vn;
         // bool inFront = (Vector3.Dot(Pt.transform.localPosition, Vn) > D); // Pt infront of the plane
@@ -75,12 +72,18 @@ public class EX_6_4_MyScript : MonoBehaviour
         float d = 0f;
         // if (inFront)
         // {
-            d = Vector3.Dot(Pt.transform.localPosition, Vn);
-            Pl.transform.localPosition = d * Vn;
-            Pon.transform.localPosition = Pt.transform.localPosition - (d - D) * Vn;
+        d = Vector3.Dot(Pt.transform.localPosition, Vn);
+        float angleDeg = Mathf.Acos(Vector3.Dot(Vn,Pt.transform.localPosition.normalized )) * Mathf.Rad2Deg;
+        Debug.Log("Angle between Vn and Pt:" + angleDeg + "Deg");
+
+        bool moreThanHalf = (angleDeg < 90f);
+        Pon.SetActive(moreThanHalf);
+
+        Pl.transform.localPosition = d * Vn;
+        Pon.transform.localPosition = Pt.transform.localPosition - (d - D) * Vn;
         // }
 
-        #region  For visualizing the vectors
+        #region For visualizing the vectors
 
         AxisFrame.ShowAxisFrame = ShowAxisFrame;
         ShowPtOnPlane.DrawVector = ShowProjections;
@@ -92,17 +95,16 @@ public class EX_6_4_MyScript : MonoBehaviour
 
         // if (!inFront)
         // {
-            d = Vector3.Dot(Pt.transform.localPosition, Vn);
-            Pon.transform.localPosition = Pt.transform.localPosition - (d - D) * Vn;
-            Pl.transform.localPosition = d * Vn;
+        d = Vector3.Dot(Pt.transform.localPosition, Vn);
+        Pon.transform.localPosition = Pt.transform.localPosition - (d - D) * Vn;
+        Pl.transform.localPosition = d * Vn;
         // }
 
         float offset = 1.5f;
         float Dsize = Mathf.Abs(D) + offset;
         Vector3 from = Vector3.zero;
 
-        if (D < 0)
-        {
+        if (D < 0){
             from = D * Vn;
             Dsize = Mathf.Abs(D) + offset;
         }
@@ -110,15 +112,12 @@ public class EX_6_4_MyScript : MonoBehaviour
         float useD = Mathf.Max(d, Vector3.Dot(Vn, (Pt.transform.localPosition - Pn.transform.localPosition)));
         float useSize = Dsize;
         // now consider d
-        if ((useD + offset) > Dsize)
-        {
+        if ((useD + offset) > Dsize){
             useSize = useD + offset;
         }
-        else if (useD < 0)
-        {
+        else if (useD < 0){
             Vector3 toFrom = Pl.transform.localPosition - from;
-            if (Vector3.Dot(toFrom, Vn) < 0f)
-            {
+            if (Vector3.Dot(toFrom, Vn) < 0f){
                 from = Pl.transform.localPosition;
                 useSize = Dsize + Pl.transform.localPosition.magnitude;
             }
@@ -142,11 +141,11 @@ public class EX_6_4_MyScript : MonoBehaviour
         ShowPtOnPlane.VectorFromTo(Pon.transform.localPosition, Pt.transform.localPosition);
         Pon.transform.localRotation = Quaternion.FromToRotation(Vector3.up, Vn);
 
-        ShowPlane.PlaneNormal= -Vn;
+        ShowPlane.PlaneNormal = -Vn;
 
         ShowPlane.Center = Pn.transform.localPosition;
         ShowPlane.XSize = ShowPlane.ZSize = s;
-        #endregion
 
+        #endregion
     }
 }
