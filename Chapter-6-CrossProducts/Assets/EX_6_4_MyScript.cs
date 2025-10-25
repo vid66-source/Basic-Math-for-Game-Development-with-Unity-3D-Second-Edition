c817b970d86ae4a42ecaba54473f3f128992f503
vid66 <v.khristoliubov@red-beat.com>
2025-10-26 00:45:08 +0300

    Projection with 2D Bound Inside-Outside Test
    My Solution Without AI Help

--- Chapter-6-CrossProducts/Assets/EX_6_4_MyScript.cs
+++ Chapter-6-CrossProducts/Assets/EX_6_4_MyScript.cs
@@ -6,6 +6,8 @@ public class EX_6_4_MyScript : MonoBehaviour {
     public bool ShowAxisFrame = true;
     public bool ShowProjections = true;
     public bool DefinePlaneByD;
+    public bool DefinePlanePn;
+    public bool DefinePlaneP0P1P2;
 
     // Plane Equation: P dot Vn = D
     public Vector3 Vn = Vector3.up;
@@ -16,31 +18,54 @@ public class EX_6_4_MyScript : MonoBehaviour {
     public GameObject Pt = null; // The point to be projected onto the plane
     public GameObject Pl = null; // Projection of Pt on Vn
     public GameObject Pon = null; // Projection of Pt on the plane
+    public GameObject P0 = null;
+    public GameObject P1 = null;
+    public GameObject P2 = null;
 
     #region For visualizing the vectors
 
-    private MyVector ShowNormal, ShowPt;
+    private MyVector ShowNormal, ShowPt, ShowP1, ShowP2, ShowP1P2Normal, ShowP1NormalNormal;
     private MyXZPlane ShowPlane; // Plane where XZ lies
     private MyLineSegment ShowPtOnPlane, ShowPtOnN;
     private Vector3 _initialScale;
     private float PtOnVnProjection;
 
+
     #endregion
 
     // Start is called before the first frame update
     void Start() {
+
         DefinePlaneByD = true;
 
         Debug.Assert(Pn != null); // Verify proper setting in the editor
         Debug.Assert(Pt != null);
         Debug.Assert(Pl != null);
         Debug.Assert(Pon != null);
+        Debug.Assert(P0 != null);
+        Debug.Assert(P1 != null);
+        Debug.Assert(P2 != null);
+        P0.SetActive(false);
+        P1.SetActive(false);
+        P2.SetActive(false);
 
         #region For visualizing the vectors
 
         // To support visualizing the vectors
         ShowNormal = new MyVector {
             VectorColor = Color.white
+        };
+        ShowP1 = new MyVector {
+            VectorColor = Color.skyBlue
+        };
+        ShowP2 = new MyVector {
+            VectorColor = Color.darkSlateBlue
+        };
+        ShowP1P2Normal = new MyVector {
+            VectorColor = Color.white
+        };
+        ShowP1NormalNormal = new MyVector {
+            VectorColor = Color.yellowNice
         };
         ShowPlane = new MyXZPlane {
             PlaneColor = new Color(0.8f, 0.3f, 0.3f, 1.0f),
@@ -59,12 +84,17 @@ public class EX_6_4_MyScript : MonoBehaviour {
             VectorColor = Color.green,
             LineWidth = 0.05f
         };
+
         var sv = UnityEditor.SceneVisibilityManager.instance;
         sv.DisablePicking(Pn, true);
         sv.DisablePicking(Pl, true);
         sv.DisablePicking(Pon, true);
         _initialScale = Pon.transform.localScale;
 
+        ShowP1.DrawVector = false;
+        ShowP2.DrawVector = false;
+        ShowP1P2Normal.DrawVector = false;
+        ShowP1NormalNormal.DrawVector = false;
         #endregion
     }
 
@@ -72,7 +102,8 @@ public class EX_6_4_MyScript : MonoBehaviour {
     void Update() {
         Vector3 VnNorm = Vn;
 
-        if (DefinePlaneByD) {
+        if (DefinePlaneByD){
+            TurnOnOffVectorsAndPoints(false);
             // Режим: користувач задає D та Vn
             Vn.Normalize();
             VnNorm = Vn;
@@ -81,7 +112,8 @@ public class EX_6_4_MyScript : MonoBehaviour {
             Pl.transform.localPosition = PtOnVnProjection * Vn;
             Pon.transform.localPosition = Pt.transform.localPosition - (PtOnVnProjection - D) * Vn;
         }
-        else {
+        else if (DefinePlanePn){
+            TurnOnOffVectorsAndPoints(false);
             // Режим: користувач маніпулює позицією Pn
             Pn.transform.localPosition = PnPos;
             D = Pn.transform.localPosition.magnitude;
@@ -95,6 +127,39 @@ public class EX_6_4_MyScript : MonoBehaviour {
             Pl.transform.localPosition = PtOnVnProjection * VnNorm;
             Pon.transform.localPosition = Pt.transform.localPosition - (PtOnVnProjection - D) * VnNorm;
         }
+        else if (DefinePlaneP0P1P2){
+            TurnOnOffVectorsAndPoints(true);
+            Vector3 v1 = P1.transform.localPosition - P0.transform.localPosition;
+            Vector3 v2 = P2.transform.localPosition - P0.transform.localPosition;
+            Vector3 P1P2Normal = -Vector3.Cross(v1, v2);
+            VnNorm = P1P2Normal.normalized;
+            Vector3 P1NormalNormal = -Vector3.Cross(v1, VnNorm);
+            D = Vector3.Dot(VnNorm, P0.transform.localPosition);
+            Pn.transform.localPosition = D * VnNorm;
+            PtOnVnProjection = Vector3.Dot(Pt.transform.localPosition, VnNorm);
+            Pl.transform.localPosition = PtOnVnProjection * VnNorm;
+            Pon.transform.localPosition = Pt.transform.localPosition - (PtOnVnProjection - D) * VnNorm;
+            ShowP1.Direction = v1;
+            ShowP1.Magnitude = v1.magnitude;
+            ShowP1.VectorFromTo(P0.transform.localPosition, P1.transform.localPosition);
+            ShowP2.Direction = v2;
+            ShowP2.Magnitude = v2.magnitude;
+            ShowP2.VectorFromTo(P0.transform.localPosition, P2.transform.localPosition);
+            ShowP1P2Normal.Direction = P1P2Normal;
+            ShowP1P2Normal.VectorAtDirLength(P0.transform.localPosition, P1P2Normal, P1P2Normal.magnitude);
+            ShowP1NormalNormal.Direction = P1NormalNormal;
+            ShowP1NormalNormal.VectorAtDirLength(P0.transform.localPosition, P1NormalNormal, P1NormalNormal.magnitude);
+            Debug.Log("V1 Length:" + v1.magnitude + ". P1NormalNormal Length:" + P1NormalNormal.magnitude);
+
+            Vector3 P0Pon = Pon.transform.localPosition - P0.transform.localPosition;
+            float P0PonProjOnP1 = Vector3.Dot(P0Pon, v1.normalized);
+            float P0PonProjOnP1NormalNormal = Vector3.Dot(P0Pon, P1NormalNormal.normalized);
+            bool boundInsideOutside = (P0PonProjOnP1 <= v1.magnitude && P0PonProjOnP1 >= 0) && (P0PonProjOnP1NormalNormal <= P1NormalNormal.magnitude && P0PonProjOnP1NormalNormal >= 0);;
+            if (boundInsideOutside){
+                Debug.Log("P0PonProjOnP1:" + P0PonProjOnP1 + ". P0PonProjOnP1NormalNormal:" + P0PonProjOnP1NormalNormal + ". Pon INSIDE 2D bound");;
+            }
+        }
+
 
         float angleDeg = Mathf.Acos(Vector3.Dot(VnNorm, Pt.transform.localPosition.normalized)) * Mathf.Rad2Deg;
         Debug.Log("Angle between Vn and Pt:" + angleDeg + "Deg");
@@ -168,5 +233,15 @@ public class EX_6_4_MyScript : MonoBehaviour {
         ShowPlane.XSize = ShowPlane.ZSize = s;
 
         #endregion
+    }
+
+    private void TurnOnOffVectorsAndPoints(bool switcher){
+        P0.SetActive(switcher);
+        P1.SetActive(switcher);
+        P2.SetActive(switcher);
+        ShowP1.DrawVector = switcher;
+        ShowP2.DrawVector = switcher;
+        ShowP1P2Normal.DrawVector = switcher;
+        ShowP1NormalNormal.DrawVector = switcher;
     }
 }
