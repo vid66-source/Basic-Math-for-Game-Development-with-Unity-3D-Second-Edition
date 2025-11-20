911b767bff4d013d0ed100107a7920cdec19fcfb
vid66 <v.khristoliubov@red-beat.com>
2025-11-20 18:59:42 +0200

    The Number of Quaternions Concatenated

--- Chapter-8-Quaternions/Assets/EX_8_2_MyScript.cs
+++ Chapter-8-Quaternions/Assets/EX_8_2_MyScript.cs
@@ -72,41 +72,45 @@ public class EX_8_2_MyScript : MonoBehaviour
         Vector4 q1 = QFromAngleAxis(Theta1, A1.transform.localPosition);
         Vector4 q2 = QFromAngleAxis(Theta2, A2.transform.localPosition);
 
-        float negAngle = -Theta;
-        float negAngle1 = -Theta1;
-        float negAngle2 = -Theta2;
+        // float negAngle = -Theta;
+        // float negAngle1 = -Theta1;
+        // float negAngle2 = -Theta2;
 
-        Vector4 qInvWithAngle = QFromAngleAxis(negAngle, A.transform.localPosition);
-        Vector4 q1InvWithAngle = QFromAngleAxis(negAngle1, A1.transform.localPosition);
-        Vector4 q2InvWithAngle = QFromAngleAxis(negAngle2, A2.transform.localPosition);
+        // Vector4 qInvWithAngle = QFromAngleAxis(negAngle, A.transform.localPosition);
+        // Vector4 q1InvWithAngle = QFromAngleAxis(negAngle1, A1.transform.localPosition);
+        // Vector4 q2InvWithAngle = QFromAngleAxis(negAngle2, A2.transform.localPosition);
 
-
-        Vector4 qInv = new Vector4(-q.x, -q.y, -q.z, q.w);
-        Vector4 q1Inv = new Vector4(-q1.x, -q1.y, -q1.z, q1.w);
-        Vector4 q2Inv = new Vector4(-q2.x, -q2.y, -q2.z, q2.w);
+        // Vector4 qInv = new Vector4(-q.x, -q.y, -q.z, q.w);
+        // Vector4 q1Inv = new Vector4(-q1.x, -q1.y, -q1.z, q1.w);
+        // Vector4 q2Inv = new Vector4(-q2.x, -q2.y, -q2.z, q2.w);
 
         Vector3 startPos = Pi.transform.localPosition;
 
         Vector3 pos_Pr = QRotation(q, startPos);
+        Pr.transform.localPosition =  pos_Pr;
+
         Vector3 pos_Pr1 = QRotation(q1, pos_Pr);
-        Vector3 pos_Pr2 = QRotation(q2, pos_Pr1);
+        // Vector3 pos_Pr2 = QRotation(q2, pos_Pr1);
 
-        Vector3 back_Pr2 = QRotation(q2Inv, pos_Pr2);
-        Vector3 back_Pr1 = QRotation(q1Inv, back_Pr2);
-        Vector3 back_Pr = QRotation(qInv, back_Pr1);
+        // Vector3 back_Pr2 = QRotation(q2Inv, pos_Pr2);
+        // Vector3 back_Pr1 = QRotation(q1Inv, back_Pr2);
+        // Vector3 back_Pr = QRotation(qInv, back_Pr1);
 
-        Pr.transform.localPosition = back_Pr;
-        Pr1.transform.localPosition = back_Pr;
-        Pr2.transform.localPosition = back_Pr;
+        Pr1.transform.localPosition = pos_Pr1;
+
+        Vector4 q1Q2Multiply = QMultiplication(q2, q1);
+
+        Vector3 pos_Pr2 = QRotation(q1Q2Multiply, pos_Pr);
+        Pr2.transform.localPosition = pos_Pr2;
 
 
         Vector4 qc = QMultiplication(q1, q);
         qc = QMultiplication(q2, qc);
         Pc.transform.localPosition = QRotation(qc, Pi.transform.localPosition);
 
-        Vector4 qcInv = QMultiplication(q1InvWithAngle, q2InvWithAngle);
-        qcInv = QMultiplication(qInvWithAngle, qcInv);
-        Pc.transform.localPosition = QRotation(qcInv, Pc.transform.localPosition);
+        // Vector4 qcInv = QMultiplication(q1InvWithAngle, q2InvWithAngle);
+        // qcInv = QMultiplication(qInvWithAngle, qcInv);
+        // Pc.transform.localPosition = QRotation(qcInv, Pc.transform.localPosition);
         #region  For visualizing the vectors
         // To avoid confusion
         A.SetActive(DrawQuaternion);
