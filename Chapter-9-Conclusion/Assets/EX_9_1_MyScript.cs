f3848895e734903ff7b34595352a4eca716ec05d
vid66 <v.khristoliubov@red-beat.com>
2025-12-30 14:57:17 +0200

    Your Own Quaternion Rotations
    Replace the Unity Quaternion.LookRotation() function with your own quaternion
    rotation functions.

--- Chapter-9-Conclusion/Assets/EX_9_1_MyScript.cs
+++ Chapter-9-Conclusion/Assets/EX_9_1_MyScript.cs
@@ -246,7 +246,16 @@ public class EX_9_1_MyScript : MonoBehaviour
                 {
                     Pa.transform.position = Pon.transform.position;
                     Adir = vr.normalized;
-                    Pa.transform.localRotation = Quaternion.LookRotation(Adir, Vector3.up);
+                    // Pa.transform.localRotation = Quaternion.LookRotation(Adir, Vector3.up);
+                    Vector3 forward = Adir.normalized;
+                    Vector3 up = Vector3.up.normalized;
+                    Vector3 right = Vector3.Cross(up, forward).normalized;
+                    up = Vector3.Cross(forward, right).normalized;
+                    Vector4 qZ = QAlignVectors(Vector3.forward, forward);
+                    Vector3 currentUp = QRotation(qZ, Vector3.up);
+                    Vector4 qUp = QAlignVectors(currentUp, up);
+                    Vector4 finalQ = QMultiplication(qUp, qZ);
+                    Pa.transform.localRotation = V4ToQ(finalQ);
                 }
 
                 // if (von.magnitude < float.Epsilon) What will happen if you do this?
@@ -324,4 +333,57 @@ public class EX_9_1_MyScript : MonoBehaviour
         */
         #endregion
     }
+
+#region Quaternion functions
+
+    Quaternion V4ToQ(Vector4 q) {
+        return new Quaternion(q.x, q.y, q.z, q.w);
+    }
+
+    Vector4 QAlignVectors(Vector3 from, Vector3 to) {
+        from.Normalize();
+        to.Normalize();
+        float dot = Mathf.Clamp(Vector3.Dot(from, to), -1f, 1f);
+        float theta = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        Vector4 q = new Vector4(0, 0, 0, 1); // Quaternion identity
+        Vector3 axis = Vector3.Cross(from, to);
+        q = QFromAngleAxis(theta, axis);
+        return q;
+    }
+
+    // axis: should be a normalized vector
+    // angle: rotation (in degrees)
+    Vector4 QFromAngleAxis(float angle, Vector3 axis) {
+        float useTheta = angle * Mathf.Deg2Rad * 0.5f;
+        float sinTheta = Mathf.Sin(useTheta);
+        float cosTheta = Mathf.Cos(useTheta);
+        axis.Normalize();
+        return new Vector4(sinTheta * axis.x, sinTheta * axis.y, sinTheta * axis.z, cosTheta);
+    }
+
+    // Computes quaternion product of q1 q2
+    Vector4 QMultiplication(Vector4 q1, Vector4 q2) {
+        Vector4 r;
+        r.x = q1.w*q2.x + q1.x*q2.w + q1.y*q2.z - q1.z*q2.y;
+        r.y = q1.w*q2.y - q1.x*q2.z + q1.y*q2.w + q1.z*q2.x;
+        r.z = q1.w*q2.z + q1.x*q2.y - q1.y*q2.x + q1.z*q2.w;
+        r.w = q1.w*q2.w - q1.x*q2.x - q1.y*q2.y - q1.z*q2.z;
+        return r;
+    }
+
+    // Rotate p based on the quaternion q
+    Vector3 QRotation(Vector4 qr, Vector3 p) {
+        Vector4 pq = new Vector4(p.x, p.y, p.z, 0);
+        Vector4 qr_inv = new Vector4(-qr.x, -qr.y, -qr.z, qr.w);
+        // q-inv: is rotate by the same axis by -theta OR
+        //        =rotate by the -axis by theta
+        // in either case: it is the above;
+
+        pq = QMultiplication(qr, pq);
+        pq = QMultiplication(pq, qr_inv);
+        return new Vector3(pq.x, pq.y, pq.z);
+    }
+
+#endregion
+
 }
